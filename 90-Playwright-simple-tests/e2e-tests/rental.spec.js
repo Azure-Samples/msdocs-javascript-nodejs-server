bbda6b3597a78354e742f3593d0c64c2d0015622
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-03-17 13:17:22 -0700

    Simple test runs - doesn't actually succeed

--- 90-Playwright-simple-tests/e2e-tests/rental.spec.js
+++ 90-Playwright-simple-tests/e2e-tests/rental.spec.js
@@ -1,10 +1,11 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
 // page is fixture coming in as context
 // codegen gives dom elements
 test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  console.log(`URL = ${process.env.TEST_URL_BASE}`)
+  await page.goto(`${process.env.TEST_URL_BASE}/`);
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
   });

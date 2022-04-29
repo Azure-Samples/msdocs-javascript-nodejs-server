9671dd6e439ffa2006027236f12bcec4c28fa3b5
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-04-29 13:11:51 -0700

    Update rental.model.js

--- 3-Add-cosmosdb-mongodb/model/rental.model.js
+++ 3-Add-cosmosdb-mongodb/model/rental.model.js
@@ -53,7 +53,7 @@ export const addRental = async (rental) => {
 // Update one rental
 // Only handles database, image changes are handled in controller
 export const updateRental = async (rental) => {
-  return await rentalsCollection.updateOne({ _id: rental.id }, { $set: rental });
+  return await rentalsCollection.updateOne({ _id: new ObjectId(rental.id) }, { $set: rental });
 };
 // Create database connection
 export const connectToDatabase = async () => {
